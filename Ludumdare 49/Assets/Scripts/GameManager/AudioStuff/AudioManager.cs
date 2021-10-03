ab12891eacb8db9e7511e36da51d59f9235ae72c
TrueTipu <59774591+TrueTipu@users.noreply.github.com>
2021-10-03 12:18:52 +0300

    audiomanager ja kansiointi
    audiomanager script tehty
    muut scriptit kansioitu

---
+++ Ludumdare 49/Assets/Scripts/GameManager/AudioStuff/AudioManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.loop = s.loop;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        s.source.Stop();
+
+
+    }
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        s.source.Play();
+
+
+    }
+}
